4bcff9c310cc03ad436bb01db4cefeb1171ce1ec
inesfproenca <inesfproenca@gmail.com>
2016-04-03 18:32:18 +0100

    Dragon documented

--- Jogo do labirinto/src/maze/logic/Dragon.java
+++ Jogo do labirinto/src/maze/logic/Dragon.java
@@ -2,37 +2,81 @@ package maze.logic;
 
 import java.util.Random;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Dragon.
+ */
 public class Dragon extends Piece{
+	
+	/** The Constant UP. */
 	private static final int UP = 0;
+	
+	/** The Constant DOWN. */
 	private static final int DOWN = 1;
+	
+	/** The Constant LEFT. */
 	private static final int LEFT = 2;
+	
+	/** The Constant RIGHT. */
 	private static final int RIGHT = 3;
+	
+	/** The Constant STAY. */
 	private static final int STAY = 4;
 	
+	/**
+	 * The Enum State representing dragon's state.
+	 */
 	public enum State{
-		AWAKE, ASLEEP, DEAD
+		
+		/** Dragon is awake and able to kill */ AWAKE, 
+		/** Dragon is asleep and temporarily unable of murder */ ASLEEP, 
+		/** Dragon is dead */ DEAD
 	}
 	
+	/** Dragon's state. */
 	private State state;
 	
+	/**
+	 * Instantiates a new awake dragon in position (x,y).
+	 *
+	 * @param x position in horizontal axis
+	 * @param y position in vertical axis
+	 */
 	public Dragon(int x, int  y){
 		super(x,y,'D');
 		state = State.AWAKE;
 	}
 
+	/**
+	 * Kills dragon
+	 */
 	public void kill(){
 		state = State.DEAD;
 		symbol = ' ';
 	}
 
+	/**
+	 * Wakes dragon up
+	 */
 	public void wakeUp(){
 		state = State.AWAKE;
 		symbol = 'D';
 	}
+	
+	/**
+	 * Makes dragon fall asleep.
+	 */
 	public void fallAsleep(){
 		state = State.ASLEEP;
 		symbol = 'd';
 	}
+	
+	/**
+	 * Updates dragon moving and or making it fall asleep/wake up randomly if necessary.
+	 *
+	 * @param mode the game's mode
+	 * @return an int representing the direction taken by the dragon STAY, UP, DOWN, RIGHT or LEFT
+	 */
 	public int update(Mode mode){
 		Random r = new Random();
 		boolean change = r.nextBoolean();
@@ -51,6 +95,12 @@ public class Dragon extends Piece{
 
 		return STAY;
 	}
+	
+	/**
+	 * Moves dragon randomly.
+	 *
+	 * @return an int representing the direction taken by the dragon STAY, UP, DOWN, RIGHT or LEFT
+	 */
 	public int move(){
 		Random r = new Random();
 		int dir = r.nextInt(4);
@@ -72,6 +122,11 @@ public class Dragon extends Piece{
 		return dir;
 	}
 
+	/**
+	 * Undoes move.
+	 *
+	 * @param dir the direction previously taken by the dragon
+	 */
 	public void undoMove(int dir){
 		switch(dir){
 		case DOWN:
@@ -88,6 +143,11 @@ public class Dragon extends Piece{
 		}
 	}
 	
+	/**
+	 * Gets the dragon's status.
+	 *
+	 * @return dragon's status
+	 */
 	public State getStatus(){
 		return state;
 	}
